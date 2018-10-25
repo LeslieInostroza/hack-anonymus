0eca0f53ca0bd87383a3accb61d2701e02bd4fe8
LeslieInostroza <leslie.ino@gmail.com>
2018-10-25 07:34:46 -0300

    se cambia icono

--- src/Componentes/Modal/Modal.js
+++ src/Componentes/Modal/Modal.js
@@ -6,7 +6,7 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import logoPng from './../../IMG/logo1.png';
-import {  CreateNewFolder, AttachFile,LiveHelp} from '@material-ui/icons';
+import {  CreateNewFolder, AttachFile,LiveHelp,Home} from '@material-ui/icons';
 import './Modal.css'
 import {Link} from 'react-router-dom';
 
@@ -65,7 +65,7 @@ class SimpleModal extends React.Component {
         >
           <div style={getModalStyle()} className={classes.paper}>
           <img className="imgLogo" src={logoPng} alt="" />
-          <MenuItem className="btn" onClick={this.handleClose} component={Inicio}>Inicio <LiveHelp className="iconos3"></LiveHelp> </MenuItem>
+          <MenuItem className="btn" onClick={this.handleClose} component={Inicio}>Inicio <Home className="iconos3"></Home> </MenuItem>
             <MenuItem className="btn" onClick={this.handleClose}>Ayuda <LiveHelp className="iconos3"></LiveHelp> </MenuItem>
             <MenuItem className="btn" onClick={this.handleClose} component={ListaCategorias}>Categorias <AttachFile className="icon"></AttachFile> </MenuItem>
             <MenuItem className="btn" onClick={this.handleClose}>Subir Archivo <CreateNewFolder className="iconos3"></CreateNewFolder></MenuItem>
