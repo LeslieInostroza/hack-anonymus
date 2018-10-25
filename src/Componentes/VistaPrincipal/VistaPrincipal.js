5bcab28f9f71e1ffec1652e6ba18f5fdc777c34e
MLoretto <loreto.henriquez.m@gmail.com>
2018-10-25 03:32:02 -0300

    Algoritmo búsqueda

--- src/Componentes/VistaPrincipal/VistaPrincipal.js
+++ src/Componentes/VistaPrincipal/VistaPrincipal.js
@@ -4,13 +4,12 @@ import RecipeReviewCard from './../Tarjeta/Tarjeta';
 import AppBar from '../AppBar/AppBar';
 import {Grid} from '@material-ui/core'
 
-
 class VistaPrincipal extends Component {
   render() {
     return (
       <div className="App">
       <AppBar />
-      
+
       <Grid container justify={'center'}>
         <Grid item>
         <RecipeReviewCard />
--- src/Router.js
+++ src/Router.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import { BrowserRouter as ReactRouter, Route, Redirect } from 'react-router-dom';
 import App from './App';
 import VistaPrincipal from './Componentes/VistaPrincipal/VistaPrincipal';
+import Subirarchivo from './Componentes/VistaPrincipal/Subirarchivo'
 class Routers extends Component {
   render(){
     return(
       <ReactRouter>
         <App>
-          <Route path="/" exac component={VistaPrincipal}></Route>
+          <Route path="/home" exac component={VistaPrincipal}></Route>
+          <Route path="/subirarchivo" component={Subirarchivo}></Route>
         </App>
       </ReactRouter>
     )
