cc824d741f8484c7c06c042e33bce470503d3488
viviana <dmpviviana@gmail.com>
2018-10-25 06:43:11 -0300

    avanze

--- src/Componentes/listaCategoria/ListaCategoria.js
+++ src/Componentes/listaCategoria/ListaCategoria.js
@@ -1,18 +1,10 @@
 import React, {Component} from 'react';
 import {Grid} from '@material-ui/core';
 import List  from './List';
-class ListaCategoria extends Component {
-  render(){
-    return(
-      <div className="App">
-      <Grid container justify={'center'}>
-        <Grid item>
-          <List />
-        </Grid>
-      </Grid>
-      </div>
-    )
-  }
-}
 
+const ListaCategoria = () => {
+  return(
+    <List />
+)
+}
 export default ListaCategoria;
